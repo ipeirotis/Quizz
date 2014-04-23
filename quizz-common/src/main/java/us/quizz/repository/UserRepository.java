545a05153da13a5430fc711c791d99d3baf0fb7f
chunhowt <chunhowt@gmail.com>
2014-04-23 11:22:42 -0700

    Readded a function in UserRepository used in an old/myinfo page.

--- quizz-common/src/main/java/us/quizz/repository/UserRepository.java
+++ quizz-common/src/main/java/us/quizz/repository/UserRepository.java
@@ -50,4 +50,36 @@ public class UserRepository extends BaseRepository<User> {
     }
     return user;
   }
+
+  public User getUseridFromCookie(HttpServletRequest req) {
+    return getUseridFromCookie(req, null);
+  }
+
+  public User getUseridFromCookie(HttpServletRequest req, HttpServletResponse resp) {
+    // Get an array of Cookies associated with this domain
+
+    String userid = null;
+    Cookie[] cookies = req.getCookies();
+    if (cookies != null) {
+      for (Cookie c : cookies) {
+        if (c.getName().equals("username")) {
+          userid = c.getValue();
+          break;
+        }
+      }
+    }
+
+    if (userid == null) {
+      userid = UUID.randomUUID().toString();
+    }
+
+    if(resp != null) {
+      Cookie username = new Cookie("username", userid);
+      username.setMaxAge(60 * 24 * 3600);
+      username.setPath("/");
+      resp.addCookie(username);
+    }
+
+    return getOrCreateUser(userid);
+  }
 }
